8ba4df8eb94347fc9e1ac822eacc42e4e2bbb5d8
cristiano <cristiano.lourenco.ao@gmail.com>
2023-11-10 17:59:42 +0100

    working at the appBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: const Align(
+          alignment: Alignment.centerRight,
+          child: Icon(Icons.location_pin),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 24),
+            child: Icon(Icons.menu),
+          )
+        ],
+        title: const Text('CityName'),
+      ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            flex: 5,
+            child: Container(color: Colors.red),
+          ),
+          Expanded(
+            child: Container(color: Colors.yellow),
+          ),
+        ],
       ),
     );
   }
